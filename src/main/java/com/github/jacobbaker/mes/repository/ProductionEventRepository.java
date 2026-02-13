e9c6698ff002ed1c14347fe59ce93eea2159d418
Jacob Baker <bakerja@udel.edu>
2026-02-13 00:01:58 -0500

    Add missing imports to repository and service classes

--- src/main/java/com/github/jacobbaker/mes/repository/ProductionEventRepository.java
+++ src/main/java/com/github/jacobbaker/mes/repository/ProductionEventRepository.java
@@ -1,6 +1,11 @@
 package com.github.jacobbaker.mes.repository;
 
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.github.jacobbaker.mes.domain.ProductionEvent;
 
 @Repository
 public interface ProductionEventRepository extends JpaRepository<ProductionEvent, UUID> {
--- src/main/java/com/github/jacobbaker/mes/service/ProductionEventService.java
+++ src/main/java/com/github/jacobbaker/mes/service/ProductionEventService.java
@@ -1,8 +1,10 @@
 package com.github.jacobbaker.mes.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.jacobbaker.mes.domain.ProductionEvent;
+import com.github.jacobbaker.mes.repository.ProductionEventRepository;
 
 @Service
 public class ProductionEventService {
